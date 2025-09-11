36f4a82ef8b5ec3b736bfbc0adfbef8a12a142fe
Dorothea87 <147050615+Dorothea87@users.noreply.github.com>
2025-09-11 17:43:12 +0100

    Operators lesson

---
+++ .idea/LinkedInLearning/ControlFlow.py
@@ -0,0 +1,3 @@
+#if/else statement
+#for loops
+#while loops
---
+++ .idea/LinkedInLearning/Operators.py
@@ -0,0 +1,35 @@
+#Arithmetic Operators
+1 * 1
+print(5 ** 2)
+print(20/5)
+print(20 / 6)
+print(20 % 6)
+
+#Arithmetic Operators with Strings
+print('string1' + 'string2')
+print('name' * 4)
+
+#Comparison Operators
+1 == 1
+4 < 5
+4 <= 5
+5 > 4
+5 >= 4
+
+#Logical Operators (and, or, not)
+print(True and True)
+print(True and False)
+
+print(True or False)
+print(False or False)
+
+print(not False)
+print(not True)
+
+
+#Membership Operators (in and not in)
+print(1 in [1, 2, 3, 4, 5])
+print(1 not in [2, 3, 4, 5])
+
+print('dog' in 'my dog is called Pumba')
+print('cat' in 'my dog is called Pumba')
