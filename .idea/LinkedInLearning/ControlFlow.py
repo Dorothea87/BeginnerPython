68c3471b8d3602c59d80f648b17865b203e4fe74
Dorothea87 <147050615+Dorothea87@users.noreply.github.com>
2025-09-12 08:02:33 +0100

    control flow lesson

--- .idea/LinkedInLearning/ControlFlow.py
+++ .idea/LinkedInLearning/ControlFlow.py
@@ -1,3 +1,25 @@
 #if/else statement
-#for loops
-#while loops
+a = True
+b = True
+c = True
+if a:
+    print('It is true!')
+    print('Also print this')
+    if b:
+        print('Both are true')
+        if c:
+            print('All three are true')
+else:
+    print('It is false!')
+print('Always print this')
+
+#for loops - iterate over iterables e.g. list
+list = [1, 2, 3, 4, 5]
+for item in list:
+    print(item)
+
+#while loops - keeps looping until boolean we pass is false
+number = 0
+while number <5:
+    print(number)
+    number = number + 1
